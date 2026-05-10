65b79ebc9f23fd200ae3c520acbf821726027036
Muhammed-Mehboob <muhammedmehboob313@gmail.com>
2026-05-10 18:39:32 +0100

    Update bibliography with LiDAR snow removal papers and generated bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,114 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "charron2018denoising": {
+        "author": "Charron, Nicholas and Phillips, Stephen and Waslander, Steven L.",
+        "booktitle": "2018 15th Conference on Computer and Robot Vision (CRV)",
+        "doi": "10.1109/CRV.2018.00043",
+        "keywords": "rule-based, snow-removal, lidar, DROR, outlier-removal",
+        "pages": "254--261",
+        "title": "De-noising of {LiDAR} Point Clouds Corrupted by Snowfall",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "hahner2022lidar": {
+        "author": "Hahner, Martin and Sakaridis, Christos and Bijelic, Mario and Heide, Felix and Yu, Fisher and Dai, Dengxin and Van Gool, Luc",
+        "booktitle": "Proceedings of the 2022 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR52688.2022.01588",
+        "keywords": "simulation, data-augmentation, snow, lidar, object-detection",
+        "pages": "16343--16353",
+        "title": "{LiDAR} Snowfall Simulation for Robust {3D} Object Detection",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "heinzler2020cnn": {
+        "author": "Heinzler, Robin and Piewak, Florian and Schindler, Philipp and Stork, Wilhelm",
+        "doi": "10.1109/LRA.2020.2972865",
+        "journal": "IEEE Robotics and Automation Letters",
+        "keywords": "deep-learning, snow-removal, lidar, WeatherNet, CNN",
+        "number": "2",
+        "pages": "2514--2521",
+        "title": "{CNN}-Based {LiDAR} Point Cloud De-Noising in Adverse Weather",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "5",
+        "year": "2020"
+    },
+    "le2022adaptive": {
+        "author": "Le, Minh-Hai and Cheng, Ching-Hwa and Liu, Don-Gey and Nguyen, Thanh-Tuan",
+        "doi": "10.3390/electronics11192993",
+        "journal": "Electronics",
+        "keywords": "rule-based, snow-removal, lidar, AGDOR, density-filter",
+        "number": "19",
+        "pages": "2993",
+        "title": "An Adaptive Group of Density Outlier Removal Filter: Snow Particle Removal from {LiDAR} Data",
+        "type": "article",
+        "volume": "11",
+        "year": "2022"
+    },
+    "park2020fast": {
+        "author": "Park, Jong-Il and Park, Jaemin and Kim, Kang-Soo",
+        "doi": "10.1109/ACCESS.2020.3020266",
+        "journal": "IEEE Access",
+        "keywords": "rule-based, snow-removal, lidar, LIOR, intensity-filter",
+        "pages": "160202--160212",
+        "title": "Fast and Accurate Desnowing Algorithm for {LiDAR} Point Clouds",
+        "type": "article",
+        "volume": "8",
+        "year": "2020"
+    },
+    "seppanen20234denoisenet": {
+        "author": "Seppanen, Alvari and Ojala, Risto and Tammi, Kari",
+        "doi": "10.1109/LRA.2022.3227863",
+        "journal": "IEEE Robotics and Automation Letters",
+        "keywords": "deep-learning, temporal, snow-removal, lidar, 4DenoiseNet",
+        "number": "1",
+        "pages": "456--463",
+        "title": "{4DenoiseNet}: Adverse Weather Denoising from Adjacent Point Clouds",
+        "type": "article",
+        "volume": "8",
+        "year": "2023"
+    },
+    "wang2022scalable": {
+        "author": "Wang, Weiqi and You, Xiong and Chen, Lingyu and Tian, Jiangpeng and Tang, Fen and Zhang, Lantian",
+        "doi": "10.3390/rs14061468",
+        "journal": "Remote Sensing",
+        "keywords": "rule-based, snow-removal, lidar, DDIOR, dynamic-filter",
+        "number": "6",
+        "pages": "1468",
+        "title": "A Scalable and Accurate De-Snowing Algorithm for {LiDAR} Point Clouds in Winter",
+        "type": "article",
+        "volume": "14",
+        "year": "2022"
+    },
+    "wang2023snow": {
+        "author": "Wang, Weimin and Yang, Ting and Du, Yu and Liu, Yu",
+        "doi": "10.1109/LRA.2023.3311360",
+        "journal": "IEEE Robotics and Automation Letters",
+        "keywords": "rule-based, probabilistic, CRF, snow-removal, lidar, CRFOR",
+        "number": "10",
+        "pages": "6739--6746",
+        "title": "Snow Removal for {LiDAR} Point Clouds With Spatio-Temporal Conditional Random Fields",
+        "type": "article",
+        "volume": "8",
+        "year": "2023"
+    },
+    "yu2022lisnownet": {
+        "author": "Yu, Ming-Yuan and Vasudevan, Ram and Johnson-Roberson, Matthew",
+        "booktitle": "2022 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)",
+        "doi": "10.1109/IROS47612.2022.9981299",
+        "keywords": "deep-learning, unsupervised, snow-removal, lidar, LiSnowNet",
+        "pages": "6820--6826",
+        "title": "{LiSnowNet}: Real-Time Snow Removal for {LiDAR} Point Cloud",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "zhang2023perception": {
+        "author": "Zhang, Yuxiao and Carballo, Alexander and Yang, Hanting and Takeda, Kazuya",
+        "doi": "10.1016/j.isprsjprs.2022.12.021",
+        "journal": "ISPRS Journal of Photogrammetry and Remote Sensing",
+        "keywords": "survey, adverse-weather, lidar, autonomous-driving",
+        "pages": "146--177",
+        "title": "Perception and Sensing for Autonomous Vehicles under Adverse Weather Conditions: A Survey",
+        "type": "article",
+        "volume": "196",
+        "year": "2023"
     }
 };

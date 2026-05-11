9edcb65164fa3b08d96a4db6015535f1f7585833
Muhammed-Mehboob <muhammedmehboob313@gmail.com>
2026-05-11 02:21:27 +0100

    Add default tag selectors for categorized filtering

--- src/js/survis.js
+++ src/js/survis.js
@@ -2,6 +2,10 @@ window.surVisVersion = '0.1.0';
 
 $(document).ready(function () {
   page.init();
+  selectors.toggleSelector('keywords', 'type:rule-based');
+  selectors.toggleSelector('keywords', 'type:deep-learning');
+  selectors.toggleSelector('keywords', 'type:simulation');
+  selectors.toggleSelector('keywords', 'type:survey');
   page.update(true);
   selectors.readQueryFromUrl();
 });
